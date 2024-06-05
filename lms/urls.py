cae86470f3e98e85882cd7c88e89ba4a3ae979ed
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-06-05 05:22:42 +0300

    -Добавление CRUD generics views для модели урока.

--- lms/urls.py
+++ lms/urls.py
@@ -1,13 +1,18 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from lms import views
 from lms.apps import LmsConfig
-from lms.views import CourseViewSet
 
 app_name = LmsConfig.name
 
 router = DefaultRouter()
-router.register(r'lms', CourseViewSet, basename='lms')
+router.register(r'lms', views.CourseViewSet, basename='lms')
 
 urlpatterns = [
-
+    path('lesson/create/', views.LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/', views.LessonListAPIView.as_view(), name='lesson-list'),
+    path('lesson/<int:pk>/', views.LessonRetrieveAPIView.as_view(), name='lesson-retrieve'),
+    path('lesson/update/<int:pk>/', views.LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lesson/delete/<int:pk>/', views.LessonDestroyAPIView.as_view(), name='lesson-delete'),
 ] + router.urls
