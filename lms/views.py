eb3ca8f94b0b1a8a82122b305249d3ed306b1b5f
Sergey Shiryaev <shiryaev_fox@mail.ru>
2024-06-05 03:55:46 +0300

    -Добавление представления для CRUD курса.

--- lms/views.py
+++ lms/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+from rest_framework import generics, viewsets
+from lms.serializers import CourseSerializer, LessonSerializer
+from lms.models import Course, Lesson
 
-# Create your views here.
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
